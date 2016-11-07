a8fd9f11278dd0a5d6d706d426f7b34cdc2959c0
Ruairidh Finnigan <ruairidhfinnigan@gmail.com>
2016-11-07 18:01:48 +1100

    Create MovieListFragment

--- app/src/main/java/com/rfinnigan/popular_movies/MainActivity.java
+++ app/src/main/java/com/rfinnigan/popular_movies/MainActivity.java
@@ -5,9 +5,18 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final String LOG_TAG= MainActivity.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new MovieListFragment())
+                    .commit();
+        }
     }
 }
---
+++ app/src/main/java/com/rfinnigan/popular_movies/MovieListFragment.java
@@ -0,0 +1,10 @@
+package com.rfinnigan.popular_movies;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by ruairidhfinnigan on 07/11/2016.
+ */
+
+public class MovieListFragment extends Fragment {
+}
