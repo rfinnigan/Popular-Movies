3a3bb3c24221770cba3fd77b507b6783e40f930b
Ruairidh Finnigan <ruairidhfinnigan@gmail.com>
2016-11-08 16:31:27 +1100

    Add settings activity to set API Key

--- app/src/main/java/com/rfinnigan/popular_movies/MainActivity.java
+++ app/src/main/java/com/rfinnigan/popular_movies/MainActivity.java
@@ -1,7 +1,10 @@
 package com.rfinnigan.popular_movies;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,5 +21,31 @@ public class MainActivity extends AppCompatActivity {
                     .add(R.id.container, new MovieListFragment())
                     .commit();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
+            return true;
+        }
+
+
+
+        return super.onOptionsItemSelected(item);
     }
 }
